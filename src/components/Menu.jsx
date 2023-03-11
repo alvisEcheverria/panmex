8db168144d09aec917c1767b80558809bb4948aa
Alvis <alvis.cm@gmail.com>
2023-03-10 22:31:08 -0500

    se agregan nuevos productos y cambios en los precios del menu, se baja el peso de las imagenes al convertirlas en webp

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -50,45 +50,99 @@ const Menu = () => {
         }
 
     }
+
+    const chilaquilesAndSopes = (products) =>{
+        if(products.name === "Chilaquiles de Pollo en Salsa Verde o Roja"){
+            return <>
+                        <li><p><span className='size-price-chila-sop'>{products.single}</span> ${products.singlePrice.toFixed(2)}</p></li>
+                        <li><p><span className='size-price-chila-sop'>{products.chicken}</span> ${products.chickenPrice.toFixed(2)}</p></li>
+                        <li><p><span className='size-price-chila-sop'>{products.bistec}</span> ${products.bistecPrice.toFixed(2)}</p></li>
+                        <li><p><span className='size-price-chila-sop'>{products.egg}</span> ${products.eggPrice.toFixed(2)}</p></li>
+                    </>
+        }
+        else if(products.name === "Sopes de Frijol"){
+            return  <>
+                        <li><p><span className='size-price-chila-sop'>{products.single}</span> ${products.singlePrice.toFixed(2)}</p></li>
+                        <li><p><span className='size-price-chila-sop'>{products.combined}</span> ${products.combinedPrice.toFixed(2)}</p></li>
+                    </>
+        }
+    }
     
     return (
         <section id='menu'>
             <div className='menu-container'>
-                <h2 className='title-menu'>Nuestro Menu</h2>
-                <ul className='icons-container'>
-                    {
-                        menu.map((menus, index) =>(
-                            <li className='icon-flex'
-                                style={index > 0? {borderLeft: 'dashed 0.1rem gray'}: null}  
-                                onClick={()=> handleProduct(index)} 
-                                key={menus.products}
-                            >
-                                <i className={`${menus.productImg}`}></i>
-                                <h3 style={indexMenu === index ? { color: '#d62828' } : null}>{menus.products}</h3>
-                            </li>
-                        ))
-                    }
-                </ul>
+                <div className='items-content'>
+                    <h2 className='title-menu'>Nuestro Menu</h2>
+                    <ul className='icons-container'>
+                        {
+                            menu.map((menus, index) =>(
+                                <li className='icon-flex'
+                                    style={index > 0? {borderLeft: 'dashed 0.1rem gray'}: null}  
+                                    onClick={()=> handleProduct(index)} 
+                                    key={menus.products}
+                                >
+                                    <i className={`${menus.productImg}`}></i>
+                                    <h3 style={indexMenu === index ? { color: '#d62828' } : null}>{menus.products}</h3>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
                 <div className='slider-card-content'>
-                    <button className={`btn-menu ${startIndex === 0 && 'btn-disabled'}`} onClick={handlePrev} disabled={startIndex === 0}><i className="fa-solid fa-caret-left"></i></button>
+                    <button className={`btn-menu btn-menu-left ${startIndex === 0 && 'btn-disabled'}`} onClick={handlePrev} disabled={startIndex === 0}><i className="fa-solid fa-caret-left"></i></button>
                     <ul className='card-menu-container'>
                         {
                             mobile()?.map(products =>(
                                 <li className='card-menu' key={products.name}>
-                                    <img className='img-product' src={products.img} alt="producto" />
+                                    <div className='img-menu-content'>
+                                        <img className='img-product' src={products.img} alt="producto" />
+                                    </div>
                                     <div className='product-description-container'>
-                                        <div>
+                                        <div className='description-container'>
                                             <h3 className='product-name'>{products.name}</h3>
+                                            <p className='description-product'>{products.first}</p>
+                                            <p className='description-product'>{products.salad}</p>
+                                            <p className='description-product'>{products.second}</p>
                                             <p className='description-product'>{products.description}</p>
                                         </div>
-                                        <p className='price'>${products.bigPrice?.toFixed(2)}</p>
+                                        <div className='price-container'>
+                                            <ul className='chilaquil-sopes-content'>
+                                                {chilaquilesAndSopes(products)}
+                                            </ul>
+                                            {
+                                                indexMenu === 0&&
+                                                    <>
+                                                        {
+                                                            products.smallPrice&&
+                                                                < p className='price'><span className='size'>P | </span>${products.smallPrice?.toFixed(2)}</p>
+                                                        }
+                                                        {
+                                                            products.midPrice&&
+                                                                <p className='price'><span className='size'>M | </span>${products.midPrice?.toFixed(2)}</p>
+                                                        }
+                                                        
+                                                    </>
+                                            }
+                                            <p className='price' style={indexMenu !== 0? {textAlign: 'center'}: null}>
+                                                {
+                                                    indexMenu === 0 && products.bigPrice &&
+                                                        <span className='size'>G | </span>
+                                                }
+                                                {
+                                                    products.bigPrice&&
+                                                        "$"
+                                                }
+                                                {products.bigPrice?.toFixed(2)}
+                                            </p>
+                                        </div>
                                     </div>
                                 </li>
                             )) 
                         }
                     </ul>
-                    <button className={`btn-menu ${startIndex + 6 >= menu[indexMenu]?.nameProduct.length && 'btn-disabled'}`} onClick={handleNext} disabled={startIndex + 6 >= menu[indexMenu]?.nameProduct.length}><i className="fa-solid fa-caret-right"></i></button>
-                </div>      
+                    <button className={`btn-menu btn-menu-right ${startIndex + 6 >= menu[indexMenu]?.nameProduct.length && 'btn-disabled'}`} onClick={handleNext} disabled={startIndex + 6 >= menu[indexMenu]?.nameProduct.length}><i className="fa-solid fa-caret-right"></i></button>
+                </div>
+                <p className='footer-menu'>** ofrecemos servicio para comer y llevar **</p>
             </div>
         </section>
     );
--- src/products.js
+++ src/products.js
@@ -1,87 +1,99 @@
 // Imagenes "Lo calientito "
 
-import americano from './assets/img/menu/americano.png';
-import capuchino from './assets/img/menu/capuchino.png';
-import latte from './assets/img/menu/latte.png';
-import express from './assets/img/menu/express.png';
-import capuchinoSabor from './assets/img/menu/capuchino-sabor.png';
-import chocolate from './assets/img/menu/chocolate.png';
-import te from './assets/img/menu/te.png';
+import americano from './assets/img/menu/americano.webp';
+import capuchino from './assets/img/menu/capuchino.webp';
+import latte from './assets/img/menu/latte.webp';
+import express from './assets/img/menu/express.webp';
+import capuchinoSabor from './assets/img/menu/capuchino-sabor.webp';
+import chocolate from './assets/img/menu/chocolate.webp';
+import te from './assets/img/menu/te.webp';
 
-// Imagenes "Jugos Naturales"
+// Imagenes "Jugos, aguas y refrescos"
 
-import naranja from './assets/img/menu/naranja.png';
-import mandarina from './assets/img/menu/mandarina.png';
-import toronja from './assets/img/menu/toronja.png';
-import detox from './assets/img/menu/detox.png';
-import antigrasa from './assets/img/menu/antigrasa.png';
-import amarillo from './assets/img/menu/amarillo.png';
-
-// Imagenes "Las Aguas"
-
-import jamaica from './assets/img/menu/jamaica.png';
-import tamarindo from './assets/img/menu/tamarindo.png';
-import horchata from './assets/img/menu/horchata.png';
-import colaZero from './assets/img/menu/cocacola-zero.png';
-import cocaCola from './assets/img/menu/cocacola.png';
-import sprite from './assets/img/menu/sprite.png';
-import fresca from './assets/img/menu/fresca.png';
-import sidral from './assets/img/menu/sidral.png';
-import ciel from './assets/img/menu/ciel.png';
+import naranja from './assets/img/menu/naranja.webp';
+import toronja from './assets/img/menu/toronja.webp';
+import verde from './assets/img/menu/verde.webp';
+import detox from './assets/img/menu/detox.webp';
+import antigrasa from './assets/img/menu/antigrasa.webp';
+import amarillo from './assets/img/menu/amarillo.webp';
+import tamarindo from './assets/img/menu/tamarindo.webp';
+import horchata from './assets/img/menu/horchata.webp';
+import colaZero from './assets/img/menu/cocacola-zero.webp';
+import cocaCola from './assets/img/menu/cocacola.webp';
+import sprite from './assets/img/menu/sprite.webp';
+import fresca from './assets/img/menu/fresca.webp';
+import sidral from './assets/img/menu/sidral.webp';
+import ciel from './assets/img/menu/ciel.webp';
 
 // Imagenes "La Tortería"
 
-import americana from './assets/img/menu/americana.png';
-import chilaquiles from './assets/img/menu/chilaquiles.png';
-import chorizo from './assets/img/menu/chorizo.png';
-import diva from './assets/img/menu/diva.png';
-import hawaiana from './assets/img/menu/hawaiana.png';
-import tortaHuevo from './assets/img/menu/torta-huevo.png';
-import tortaHuevoQueso from './assets/img/menu/torta-huevo-conqueso.png';
-import tortaJamon from './assets/img/menu/torta-jamon.png';
-import tortaJamonQueso from './assets/img/menu/torta-jamon-queso.png';
-import tortaMilanesa from './assets/img/menu/torta-milanesa.png';
-import milanesaQueso from './assets/img/menu/milanesa-queso.png';
-import tortaRes from './assets/img/menu/torta-res.png';
-import tortaResQueso from './assets/img/menu/torta-res-queso.png';
-import tortaNorteña from './assets/img/menu/torta-norteña.png';
-import tortaPierna from './assets/img/menu/torta-pierna.png';
-import tortaPiernaQueso from './assets/img/menu/torta-pierna-queso.png';
-import tortaRusa from './assets/img/menu/torta-rusa.png';
-import tortaSalchicha from './assets/img/menu/torta-salchicha.png';
-import tortaSalchichaQueso from './assets/img/menu/torta-salchicha-queso.png';
-import tortaStar from './assets/img/menu/torta-star.png';
+import americana from './assets/img/menu/americana.webp';
+import chilaquiles from './assets/img/menu/chilaquiles.webp';
+import chorizo from './assets/img/menu/chorizo.webp';
+import diva from './assets/img/menu/diva.webp';
+import hawaiana from './assets/img/menu/hawaiana.webp';
+import tortaHuevo from './assets/img/menu/torta-huevo.webp';
+import tortaHuevoQueso from './assets/img/menu/torta-huevo-conqueso.webp';
+import tortaJamon from './assets/img/menu/torta-jamon.webp';
+import tortaJamonQueso from './assets/img/menu/torta-jamon-queso.webp';
+import tortaMilanesa from './assets/img/menu/torta-milanesa.webp';
+import milanesaQueso from './assets/img/menu/milanesa-queso.webp';
+import tortaCubana from './assets/img/menu/torta-cubana.webp';
+import tortaRes from './assets/img/menu/torta-res.webp';
+import tortaResQueso from './assets/img/menu/torta-res-queso.webp';
+import tortaNorteña from './assets/img/menu/torta-norteña.webp';
+import tortaPierna from './assets/img/menu/torta-pierna.webp';
+import tortaPiernaQueso from './assets/img/menu/torta-pierna-queso.webp';
+import tortaRusa from './assets/img/menu/torta-rusa.webp';
+import tortaSalchicha from './assets/img/menu/torta-salchicha.webp';
+import tortaSalchichaQueso from './assets/img/menu/torta-salchicha-queso.webp';
+import tortaStar from './assets/img/menu/torta-star.webp';
 
 // Imagenes "Los Sandwiches"
 
-import hamburguesa from './assets/img/menu/hamburguesa.png';
-import hotDog from './assets/img/menu/hot-dog.png';
-import sandwich from './assets/img/menu/sandwich.png';
-import croissant from './assets/img/menu/croissant.png';
-import hojaldra from './assets/img/menu/hojaldra.png';
-import bollosRellenos from './assets/img/menu/bollos-rellenos.png';
+import hamburguesa from './assets/img/menu/hamburguesa.webp';
+import sandwich from './assets/img/menu/sandwich.webp';
+import croissant from './assets/img/menu/croissant.webp';
+import hojaldra from './assets/img/menu/hojaldra.webp';
+import bollosRellenos from './assets/img/menu/bollos-rellenos.webp';
 
 // Imagenes "Los Desayunos"
 
-import granola from './assets/img/menu/granola.png';
-import enchiladas from './assets/img/menu/enchiladas.png';
-import chilaquilesPollo from './assets/img/menu/chilaquiles-pollo.png';
-import flautas from './assets/img/menu/flautas.png';
-import sopesFrijol from './assets/img/menu/sopes-frijol.png';
-import tamales from './assets/img/menu/tamales-oaxaqueños.png';
-import huevosGusto from './assets/img/menu/huevos-gusto.png';
-import sincronizadas from './assets/img/menu/sincronizadas.png';
-import hotCakes from './assets/img/menu/hot-cakes.png';
-import molletes from './assets/img/menu/molletes.png';
-import milanesa from './assets/img/menu/milanesa.png';
-import papas from './assets/img/menu/papas.png';
-import ensaladas from './assets/img/menu/ensaladas.png';
+import granola from './assets/img/menu/granola.webp';
+import enchiladas from './assets/img/menu/enchiladas.webp';
+import chilaquilesPollo from './assets/img/menu/chilaquiles-pollo.webp';
+import flautas from './assets/img/menu/flautas.webp';
+import sopesFrijol from './assets/img/menu/sopes-frijol.webp';
+import tamales from './assets/img/menu/tamales-oaxaqueños.webp';
+import huevosGusto from './assets/img/menu/huevos-gusto.webp';
+import sincronizadas from './assets/img/menu/sincronizadas.webp';
+import hotCakes from './assets/img/menu/hot-cakes.webp';
+import molletes from './assets/img/menu/molletes.webp';
+import milanesa from './assets/img/menu/milanesa.webp';
+import papas from './assets/img/menu/papas.webp';
+import ensaladas from './assets/img/menu/ensaladas.webp';
+import frijoles from './assets/img/menu/frijoles.webp';
+import enchiladasSuizas from './assets/img/menu/enchiladas-suizas.webp';
+
+// Imagenes "Platos Fuertes"
+
+import tresTiempos from './assets/img/menu/tres-tiempos.webp';
+import boneless from './assets/img/menu/boneless.webp';
+import bistecPure from './assets/img/menu/bistec-pure.webp';
+import nuggetsPapas from './assets/img/menu/nuggets-papas.webp';
+import milanesaArroz from './assets/img/menu/milanesa-de-pollo-arroz.webp';
+import arrozLeche from './assets/img/menu/arroz-con-leche.webp';
 
 const menu = [
     {
         products: 'Lo Calientito',
         productImg: "fa-solid fa-mug-hot",
         nameProduct: [
+            {
+                name: 'Café Express',
+                smallPrice: 25.00,
+                img: express,
+            },
             {
                 name: 'Café Americano',
                 smallPrice: 25.00,
@@ -91,29 +103,32 @@ const menu = [
             },
             {
                 name: 'Café Capuchino',
+                description: 'Tipos de leche: entera, deslactosada y light.',
+                smallPrice: 25.00,
                 midPrice: 35.00,
                 bigPrice: 45.00,
                 img: capuchino,
             },
             {
                 name: 'Café Latte',
+                description: 'Tipos de leche: entera, deslactosada y light.',
+                smallPrice: 25.00,
                 midPrice: 35.00,
                 bigPrice: 45.00,
                 img: latte,
             },
             {
-                name: 'Café Express',
-                midPrice: 25.00,
-                img: express,
-            },
-            {
                 name: 'Café Capuchino de Sabores',
+                description: 'Tipos de leche: entera, deslactosada y light.',
+                smallPrice: 25.00,
                 midPrice: 35.00,
                 bigPrice: 45.00,
                 img: capuchinoSabor,
             },
             {
                 name: 'Chocolate Caliente',
+                description: 'Tipos de leche: entera, deslactosada y light.',
+                smallPrice: 25.00,
                 midPrice: 35.00,
                 bigPrice: 45.00,
                 img: chocolate,
@@ -125,11 +140,10 @@ const menu = [
                 bigPrice: 30.00,
                 img: te,
             }
-
         ]
     },
     {
-        products: 'Jugos Naturales',
+        products: 'Jugos, aguas y refrescos',
         productImg: "fa-solid fa-blender",
         nameProduct: [
             {
@@ -138,46 +152,15 @@ const menu = [
                 img: naranja,
             },
             {
-                name: 'Mandarina',
-                description: 'Solo en temporada',
-                bigPrice: 30.00,
-                img: mandarina,
-            },
-            {
                 name: 'Toronja',
                 bigPrice: 35.00,
                 img: toronja,
             },
             {
-                name: 'Detox',
-                description: 'Mango y manzana verde',
-                bigPrice: 42.00,
-                img: detox,
-            },
-            {
-                name: 'El Antigrasa',
-                description: 'Papaya, toronja y nopal',
-                bigPrice: 42.00,
-                img: antigrasa,
-            },
-            {
-                name: 'Bomba Amarilla',
-                description: 'Naranja, platano, mango, guayaba, toronja, mandarina, pera, manzana y avena',
-                bigPrice: 42.00,
-                img: amarillo,
-            }
-        ]
-    },
-    {
-        products: 'Las Aguas',
-        productImg: "fa-solid fa-bottle-water",
-        nameProduct: [
-            {
-                name: 'Jamaica',
-                smallPrice: null,
-                midPrice: null,
-                bigPrice: 15.00,
-                img: jamaica,
+                name: 'Verde',
+                description: '',
+                bigPrice: 30.00,
+                img: verde,
             },
             {
                 name: 'Tamarindo',
@@ -190,6 +173,24 @@ const menu = [
                 midPrice: null,
                 bigPrice: 15.00,
                 img: horchata,
+            },
+            {
+                name: 'Detox',
+                description: 'Te ayudará a limpiar por completo las toxinas que pudieron haber quedado en el organismo, toma un vaso al día. (Mango y manzana verde).',
+                bigPrice: 42.00,
+                img: detox,
+            },
+            {
+                name: 'El Antigrasa',
+                description: 'Cuídate y quema grasas con esta combinación de frutas de forma fácil y rápida. (Papaya, toronja, nopal).',
+                bigPrice: 42.00,
+                img: antigrasa,
+            },
+            {
+                name: 'Bomba Amarilla',
+                description: 'Un jugo para empezar con mucha energia el día. (Naranja, platano, mango, guayaba, toronja, mandarina, pera, manzana y avena)',
+                bigPrice: 42.00,
+                img: amarillo,
             },
             {
                 name: 'Coca Cola Zero',
@@ -314,6 +315,13 @@ const menu = [
                 img: tortaResQueso,
             },
             {
+                name: 'Cubana',
+                description: 'Bisteck de res, pierna, salchichas, jamon, queso panela y queso oaxaca',
+                midPrice: null,
+                bigPrice: 80.00,
+                img: tortaCubana,
+            },
+            {
                 name: 'Norteña',
                 description: 'Pierna, chorizo y queso manchego',
                 midPrice: null,
@@ -373,13 +381,6 @@ const menu = [
                 img: hamburguesa,
             },
             {
-                name: 'Hot Dog',
-                description: 'Salchicha de pavo con chile jalapeño, cebolla y jitomate',
-                midPrice: null,
-                bigPrice: 35.00,
-                img: hotDog,
-            },
-            {
                 name: 'Sándwich',
                 description: 'Jamón con queso amarillo, cebolla, jitomate, lechuga romana y chiles en vinagre',
                 midPrice: null,
@@ -423,19 +424,24 @@ const menu = [
             {
                 name: 'Enchiladas de Pollo en Salsa Verde o Roja',
                 description: 'Acompañados con porción individual de frijoles refritos, cebolla y queso',
-                midPrice: null,
                 bigPrice: 70.00,
                 img: enchiladas,
             },
             {
                 name: 'Chilaquiles de Pollo en Salsa Verde o Roja',
                 description: 'Acompañados con porción individual de frijoles refritos, cebolla y queso',
-                midPrice: 59.00,
-                bigPrice: 69.00,
+                single: "Sencillos",
+                chicken: "Pollo",
+                bistec: "Bistec",
+                egg: "Huevo",
+                singlePrice: 59.00,
+                chickenPrice: 69.00,
+                bistecPrice: 69.00,
+                eggPrice: 69.00,
                 img: chilaquilesPollo,
             },
             {
-                name: 'Flautas de Pollo',
+                name: 'Flautas de Pollo o Res',
                 description: 'Tres flautas servidas en cama de lechuga romana con jitomate y cebolla',
                 bigPrice: 70.00,
                 img: flautas,
@@ -443,50 +449,52 @@ const menu = [
             {
                 name: 'Sopes de Frijol',
                 description: 'Frijoles refritos con chorizo, queso y crema',
-                midPrice: 60.00,
-                bigPrice: 70.00,
+                single: "Sencillos",
+                combined: "Combinados",
+                singlePrice: 60.00,
+                combinedPrice: 70.00,
                 img: sopesFrijol,
             },
             {
                 name: 'Tamales oaxaqueños o de maíz, verdes o rojos',
                 description: 'En hoja de platano',
-                midPrice: null,
                 bigPrice: 35.00,
                 img: tamales,
             },
             {
                 name: 'Huevos al gusto',
                 description: 'Revueltos, estrellados, fritos o rancheros',
-                midPrice: null,
                 bigPrice: 50.00,
                 img: huevosGusto,
             },
             {
                 name: 'Sincronizadas',
                 description: 'Jamón de pierna o pavo y queso manchego en tortilla de harina',
-                midPrice: null,
                 bigPrice: 55.00,
                 img: sincronizadas,
             },
             {
                 name: 'Hot Cakes',
                 description: 'Dos hotcakes con miel de maple o abeja',
-                midPrice: null,
                 bigPrice: 50.00,
                 img: hotCakes,
             },
             {
                 name: 'Molletes',
                 description: 'De jamón o tocino',
-                midPrice: null,
                 bigPrice: 50.00,
                 img: molletes,
             },
             {
                 name: 'Milanesa Asada',
-                midPrice: null,
                 bigPrice: 66.00,
                 img: milanesa,
+            },
+            {
+                name: 'Orden de frijoles',
+                midPrice: null,
+                bigPrice: 0,
+                img: frijoles,
             },
             {
                 name: 'Papas a la Francesa',
@@ -500,6 +508,55 @@ const menu = [
                 midPrice: null,
                 bigPrice: 65.00,
                 img: ensaladas,
+            },
+            {
+                name: 'Enchiladas suizas',
+                description: 'Pollo, tomates verdes y queso manchego',
+                midPrice: null,
+                bigPrice: 0,
+                img: enchiladasSuizas,
+            }
+        ]
+    },
+    {
+        products: 'Platos Fuertes',
+        productImg: "fa-solid fa-utensils",
+        nameProduct: [
+            {
+                name: 'Menu a tres tiempos:',
+                first: '- Sopa o consomé',
+                salad: '- Arroz o ensalada',
+                second: "- Tres guisados (uno a elegir)",
+                bigPrice: 0,
+                img: tresTiempos
+            },
+            {
+                name: 'Boneless con pure o ensalada',
+                description: 'Lechuga, pepino y zanahoria.',
+                bigPrice: 0,
+                img: boneless,
+            },
+            {
+                name: 'Bistec con pure, papas fritas o ensalada',
+                bigPrice: 0,
+                img: bistecPure,
+            },
+            {
+                name: 'Papas fritas con nuggets',
+                bigPrice: 0,
+                img: nuggetsPapas,
+            },
+            {
+                name: 'Milanesa de carne o pollo con arroz',
+                description: 'Ensalada a elección',
+                bigPrice: 0,
+                img: milanesaArroz,
+            },
+            {
+                name: 'Los platos fuertes incluyen un postre a elección:',
+                first: '- Arroz con leche',
+                second: '- Platano frito con mermelada o crema',
+                img: arrozLeche
             }
         ]
     }
